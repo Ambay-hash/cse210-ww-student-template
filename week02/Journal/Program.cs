ffd4d538bb7008b3ecacb2f4e6a1c99273bd8d75
Ambay-hash <roshamiri71@gmail.com>
2025-02-05 16:58:42 +0300

    new change 3

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -1,62 +1,72 @@
 using System;
-using System.Collections.Generic;
-using System.IO;
 
-public class Journal
+class Program
 {
-    public List<Entry> Entries { get; set; }
+     public static void Main(string[] args)
+    {
+        Journal journal = new Journal();
+        string[] prompts = {
+            "Have l read the book of Mormon today?",
+            "How have l seen the hand of the Lord in my life today?",
+            "Who was the most interesting person I interacted with today?",
+            "What was the best part of my day?",
+            "What did l enjoyed today most",
+            "what was nice throgh the day",
+            "What was the strongest emotion I felt today?",
+            "Have l spend time with my family today?",
+            "If l had one thing I could do over today, what would it be?"
 
-    public Journal()
-    {
-        Entries = new List<Entry>();
-    }
-    public void AddEntry(string prompt, string response)
-    {
-        var entry = new Entry(prompt, response);
-        Entries.Add(entry);
-    }
-    public void DisplayEntries()
-    {
-        foreach (var entry in Entries)
+        };
+
+        while (true)
         {
-            Console.WriteLine(entry);
-        }
-    }
-    public void SaveToFile(string filename)
-    {
-        using (StreamWriter writer = new StreamWriter(filename))
-        {
-            foreach (var entry in Entries)
+            Console.WriteLine("Hello, Welcome to the Journal Program!");
+            Console.WriteLine("Please select one of the following choices");
+            Console.WriteLine("1. Write in your journal");
+            Console.WriteLine("2. Display your journal");
+            Console.WriteLine("3. Save the your journal to a file");
+            Console.WriteLine("4. Load your journal from a file");
+            Console.WriteLine("5. Exit");
+            Console.Write("What would you like to do today, please choose an option: ");
+
+            string option = Console.ReadLine();
+
+            if (option == "1")
             {
-                writer.WriteLine($"{entry.Date}|{entry.Prompt}|{entry.Response}");
+                Random random = new Random();
+                int index = random.Next(prompts.Length);
+                string prompt = prompts[index];
+                Console.WriteLine(prompt);
+                string response = Console.ReadLine();
+                journal.AddEntry(prompt, response);
+            }
+            else if (option == "2")
+            {
+                journal.DisplayEntries();
+            }
+            else if (option == "3")
+            {
+                Console.Write("Please enter filename to save journal: ");
+                string filename = Console.ReadLine();
+                journal.SaveToFile(filename);
+                Console.WriteLine("your Journal has been saved successfully.");
+            }
+            else if (option == "4")
+            {
+                Console.Write("Enter filename to load journal: ");
+                string filename = Console.ReadLine();
+                journal.LoadFromFile(filename);
+                Console.WriteLine("Journal was loaded successfully.");
+            }
+            else if (option == "5")
+            {
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Invalid option, please try again with correct option.");
             }
         }
     }
-    public void LoadFromFile(string filename)
-    {
-        if (File.Exists(filename))
-        {
-            Entries.Clear();
-            using (StreamReader reader = new StreamReader(filename))
-            {
-                string line;
-                while ((line = reader.ReadLine()) != null)
-                {
-                    var parts = line.Split('|');
-                    if (parts.Length == 3)
-                    {
-                        var entry = new Entry(parts[1], parts[2])
-                        {
-                            Date = parts[0]
-                        };
-                        Entries.Add(entry);
-                    }
-                }
-            }
-        }
-        else
-        {
-            Console.WriteLine("File not found.");
-        }
-    }
-}
+
+}  program.cs
