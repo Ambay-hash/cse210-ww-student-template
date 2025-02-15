0a3b665388f60528c8eb72a494af95f00ad678a8
Ambay-hash <roshamiri71@gmail.com>
2025-02-15 21:10:50 +0300

    new changes

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -7,93 +7,122 @@ class Program
         Console.WriteLine("Hello World! This is the ScriptureMemorizer Project.");
     }
 }
-using System;
+
+   using System;
 using System.Collections.Generic;
-
-public class Video
-{
-     public string Title { get; set; }
-    public string Author { get; set; }
-    public int LengthInSeconds { get; set; }
-    private List<Comment> comments;
-    public Video(string title, string author, int lengthInSeconds)
-    {
-        Title = title;
-        Author = author;
-        LengthInSeconds = lengthInSeconds;
-        comments = new List<Comment>();
-    }
-    public void AddComment(Comment comment)
-    {
-        comments.Add(comment);
-    }
-    public int GetNumberOfComments()
-    {
-        return comments.Count;
-    }
-
-    public List<Comment> GetComments()
-    {
-        return comments;
-    }
-}
-
-public class Comment
-{
-    public string CommenterName { get; set; }
-    public string Text { get; set; }
-
-    public Comment(string commenterName, string text)
-    {
-        CommenterName = commenterName;
-        Text = text;
-    }
-}
+using System.Linq;
 
 class Program
 {
     static void Main(string[] args)
     {
-    // Creating a list of videos
-        List<Video> videos = new List<Video>();
-
-     // Create video instances and adding comments
-       
-        Video video1 = new Video(" Excel tutorial for beginners", "Kevin Stratvert", 400);
-        video1.AddComment(new Comment("Menya Isaac", "Awesome thank you very much."));
-        video1.AddComment(new Comment("Ugoma Gloria", "it has taught me what l didnt know."));
-        video1.AddComment(new Comment("Ronald Bamutesiza", "Perfect, this is what l was looking for."));
-        videos.Add(video1);
-
-        Video video2 = new Video("Python for beginners", "Mosh Allans", 460);
-        video2.AddComment(new Comment("Anigo agnes", "Very helpfull, thanks so much."));
-        video2.AddComment(new Comment("Marvin Byansi", "I love programming."));
-        video2.AddComment(new Comment("Majorie Nakato", " Super great explanation of polymorphism."));
-        
-        videos.Add(video2);
-
-        Video video3 = new Video("Navigating Crypto", "Blum Ketra", 520);
-        video3.AddComment(new Comment("Komaketch Ivan", "I Love this video it's very educative."));
-        video3.AddComment(new Comment("James Balyomusi", "l really love trading."));
-        video3.AddComment(new Comment("Opira Joseph", "This is what l was looking for."));
-        video3.AddComment(new Comment("Alpha Ayemba", "This mazing video it's super nice."));
-        videos.Add(video3);
-
-        // Iterating through the videos and displaying inf
-        foreach (var video in videos)
+        // The List of scriptures to memorize
+        var scriptures = new List<Scripture>
         {
-            Console.WriteLine($"Title: {video.Title}");
-            Console.WriteLine($"Author: {video.Author}");
-            Console.WriteLine($"Length: {video.LengthInSeconds} seconds");
-            Console.WriteLine($"Number of Comments: {video.GetNumberOfComments()}");
-            Console.WriteLine("Comments:");
+            new Scripture(new Reference("1 Nephi 3:7"),
+                "And it came to pass that I, Nephi, said unto my father, I will go and do the things which the Lord hath commanded, for I know that the Lord giveth no commandments unto the children of men, save he shall prepare a way for them that they may accomplish the thing which he commandeth them."),
 
-            foreach (var comment in video.GetComments())
+            new Scripture(new Reference("2 Nephi 28:30"),
+                "For behold, thus saith the Lord God: I will give unto the children of men line upon line, precept upon precept, here a little and there a little; and blessed are those who hearken unto my precepts, and lend an ear unto my counsel, for they shall learn wisdom; for unto him that receiveth I will give more; and from them that shall say, We have enough, from them shall be taken away even that which they have."),
+                
+            new Scripture(new Reference("1 Nephi 33:1"),
+                "And now I, Nephi, cannot write all the things which were taught among my people; neither am I mighty in writing, like unto speaking; for when a man speaketh by the power of the Holy Ghost the power of the Holy Ghost carrieth it unto the hearts of the children of men."),
+        };
+
+        // This will Randomly select a scripture
+        var rand = new Random();
+        var selectedScripture = scriptures[rand.Next(scriptures.Count)];
+
+        selectedScripture.DisplayScripture();
+
+        while (!selectedScripture.IsComplete())
+        {
+            var input = Console.ReadLine().Trim().ToLower();
+
+            if (input == "quit")
             {
-                Console.WriteLine($"- {comment.CommenterName}: \"{comment.Text}\"");
+                break;
             }
+            else if (input == "")
+            {
+                selectedScripture.HideRandomWord();
+                selectedScripture.DisplayScripture();
+            }
+        }
 
-            Console.WriteLine(); 
-        }
-    }
+        // This will hidden scripture display
+        selectedScripture.DisplayScripture();
+        Console.WriteLine("Memorization complete!");
+    }
 }
+
+public class Reference
+{
+    public string Text { get; private set; }
+
+    public Reference(string text)
+    {
+        Text = text;
+    }
+}
+
+public class Word
+{
+    public string Text { get; private set; }
+    public bool Hidden { get; private set; }
+
+    public Word(string text)
+    {
+        Text = text;
+        Hidden = false;
+    }
+
+    public string GetDisplayText()
+    {
+        return Hidden ? new string('_', Text.Length) : Text;
+    }
+
+    public void Hide()
+    {
+        Hidden = true;
+    }
+}
+
+public class Scripture
+{
+    private static readonly Random rand = new Random();
+    public Reference Reference { get; private set; }
+    public List<Word> Words { get; private set; }
+
+    public Scripture(Reference reference, string text)
+    {
+        Reference = reference;
+        Words = text.Split(' ').Select(w => new Word(w)).ToList();
+    }
+
+    public void HideRandomWord()
+    {
+        // And this will randomly select a word that is not hidden and hide it
+        var visibleWords = Words.Where(w => !w.Hidden).ToList();
+        if (visibleWords.Count > 0)
+        {
+            var wordToHide = visibleWords[rand.Next(visibleWords.Count)];
+            wordToHide.Hide();
+        }
+    }
+
+    public void DisplayScripture()
+    {
+        Console.Clear();
+
+        Console.Write(Reference.Text + ": ");
+        Console.WriteLine(string.Join(" ", Words.Select(w => w.GetDisplayText())));
+        
+        Console.WriteLine("Please press Enter to continue or Type 'quit' to finish:");
+    }
+
+    public bool IsComplete()
+    {
+        return !Words.Any(w => !w.Hidden);
+    }
+} 
